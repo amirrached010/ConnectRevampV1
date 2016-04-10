7266f38c7552c015a0d2d9dee0a36b088d58db53
Amir Rached <>
2016-04-10 15:12:29 +0200

    This is the commit for the main method where the change is in the comment block.

--- src/connect/revampv1/Main.java
+++ src/connect/revampv1/Main.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    This is the main class for ConnectRevampV1.
+    the main method is where the execution begins
+*/
 package connect.revampv1;
 
 import static connect.revampv1.Util.getLogLevel;
